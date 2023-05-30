c83a7e0ea2ac757f88042bcf2373a75ed12c7c36
Anastasia <nastya.arkanova.98@bk.ru>
2023-05-30 21:05:11 +0500

    add Task1_NaturalNumbers

---
+++ Task1_NaturalNumbers/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число N:");
+int n = int.Parse(Console.ReadLine());
+PrintNumbers(n);
+Console.ReadLine();
+static void PrintNumbers(int n)
+{
+  if(n > 0)
+  {
+    Console.WriteLine(n);
+    PrintNumbers(n - 1);
+  }
+}
