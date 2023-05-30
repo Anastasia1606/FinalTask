fbdba14746ef77df69c0d07536f8f35d83519fa3
Anastasia <nastya.arkanova.98@bk.ru>
2023-05-30 21:27:50 +0500

    add Task2_SumNumbers

---
+++ Task2_SumNumbers/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите числа M и N (M <= N):");
+int m = int.Parse(Console.ReadLine());
+int n = int.Parse(Console.ReadLine());
+int sum = 0;
+for (int i = m; i <=n; i++)
+{
+  if(i > 0)
+  {
+    sum += i;
+  }
+}
+Console.WriteLine($"Сумма натуральных элементов в промежутке от {m} до {n}: {sum}");
+Console.ReadLine();
